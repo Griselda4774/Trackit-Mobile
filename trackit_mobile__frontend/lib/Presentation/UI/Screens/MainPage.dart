5806802d38117ba9399b5ca3960980f1e39bfb40
tuanh <tuanhdao2@gmail.com>
2024-05-18 18:08:59 +0700

    Add BottomNavigationBar

---
+++ trackit_mobile__frontend/lib/Presentation/UI/Screens/MainPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/CartPage.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/HomePage.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/Profile.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Widgets/MyBottomNavBar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _page = [
+    const HomePage(),
+    const CartPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _page[_selectedIndex],
+    );
+  }
+}
--- trackit_mobile__frontend/lib/main.dart
+++ trackit_mobile__frontend/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/HomePage.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/LoginPage.dart';
+import 'package:trackit_mobile__frontend/Presentation/UI/Screens/MainPage.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainPage(),
+      routes: {
+        '/loginpage':(context) => const LoginPage(),
+        '/homepage':(context) => const HomePage(),
+        '/mainpage':(context) => const MainPage()
+      },
     );
   }
 }
